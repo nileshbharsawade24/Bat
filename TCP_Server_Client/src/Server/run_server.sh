4161e39c9c352df9256bdb4b91cbb8e036d5e5d6
nileshbharsawade24 <nileshbharsawade24@gmail.com>
2021-09-24 16:40:08 +0530

    server_start

--- TCP_Server_Client/src/Server/run_server.sh
+++ TCP_Server_Client/src/Server/run_server.sh
@@ -3,3 +3,4 @@ gcc xml_parsing.c -o xml_parsing.o -c $(xml2-config --cflags --libs)
 gcc mysqlconnect.c -o mysqlconnect.o -c `mysql_config --cflags --libs`
 gcc -o server xml_parsing.o server.o mysqlconnect.o `mysql_config --cflags --libs` $(xml2-config --cflags --libs)
 rm *.o
+./server
