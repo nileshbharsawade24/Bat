1a672b77c54691350dd4102baa03a1c53d711a97
PAVAN KOLUR123 <pavana.1si17ee23@gmail.com>
2021-10-03 21:47:45 +0530

    updated transfrom function

--- esb_proj/src/esb_app/xml_parsing.h
+++ esb_proj/src/esb_app/xml_parsing.h
@@ -3,6 +3,7 @@
 #include <libxml2/libxml/xpath.h>
 #include  <stdio.h>
 #include <stdlib.h>
+
 xmlDocPtr load_xml_doc(char *xml_file_path);
 xmlXPathObjectPtr get_nodes_at_xpath(xmlDocPtr doc, xmlChar *xpath);
 xmlChar* get_element_text(char *node_xpath, xmlDocPtr doc);
